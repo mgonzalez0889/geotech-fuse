4236c23f10aeb909609fcb6be8918ab226b73dd3
Mijail Gonzalez <mgonzalez@geotech.com.co>
2021-08-10 18:05:12 -0500

    maquetacion modulo de opciones

--- src/app/pages/settings/menu-options/grid-menu-options/grid-menu-options.component.ts
+++ src/app/pages/settings/menu-options/grid-menu-options/grid-menu-options.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {MenuOptionsService} from "../../../../core/services/menu-options.service";
 
 @Component({
   selector: 'app-grid-menu-options',
@@ -8,16 +10,38 @@ import {FormControl} from "@angular/forms";
 })
 export class GridMenuOptionsComponent implements OnInit {
   searchInputControl: FormControl = new FormControl();
+  public options$: Observable<any>;
   public show: boolean = false;
-  constructor() { }
+  constructor(
+      private menuOptionService: MenuOptionsService
+  ) { }
 
   ngOnInit(): void {
+      this.fetchMenuOption();
   }
   /**
    * @description: Abre el formulario menu opcion
    */
   public openForm(): void {
       this.show = true;
+  }
+  public closeForm(value: boolean): void {
+      this.show = value;
+  }
+  public onEdit(id: number): void {
+
+  }
+  /**
+   * @description: Elimina una opcion
+   */
+  public onDelete(id: number): void {
+
+  }
+  /**
+   * @description: Listado de opciones de menu
+   */
+  public fetchMenuOption(): void {
+      this.options$ = this.menuOptionService.getMenuOptions();
   }
 
 }
