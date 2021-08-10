65fbc47b34dc8d218d58c8eefbda1943e212eb86
Mijail Gonzalez <mijail0824@gmail.com>
2021-08-09 21:37:00 -0500

    implementacion de dialog dinamico

---
+++ src/app/core/services/helper.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {IDialogAlert, IDialogAlertResult} from "../interfaces/fuse-confirmation-config";
+import {MessageDialogComponent} from "../../shared/dialogs/message-dialog/message-dialog.component";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperService {
+
+  constructor(
+      private _matDialog: MatDialog,
+      private _snackBar: MatSnackBar
+  ) { }
+
+    public showDialogAlertOption(data: IDialogAlert): Promise<IDialogAlertResult> {
+      return this._matDialog.open(MessageDialogComponent, {
+          panelClass: 'form-dialog-event',
+          data,
+          width: '400px',
+          height: '220px',
+          disableClose: data.disableClose ? true : false
+      }).afterClosed().toPromise<IDialogAlertResult>();
+    }
+}
