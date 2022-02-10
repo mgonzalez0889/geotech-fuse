caea02e7b6556d42dbacef6a90070a311f402b90
Mijail Gonzalez <mgonzalez@geotech.com.co>
2022-02-10 17:49:12 -0500

    maquetacion de componente

--- src/app/pages/mobile-management/maintenance/maintenance.module.ts
+++ src/app/pages/mobile-management/maintenance/maintenance.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MaintenanceRoutingModule } from './maintenance-routing.module';
 import { FormMaintenanceComponent } from './form-maintenance/form-maintenance.component';
 import { GridMaintenanceComponent } from './grid-maintenance/grid-maintenance.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatTableModule} from "@angular/material/table";
 
 
 @NgModule({
@@ -11,9 +17,15 @@ import { GridMaintenanceComponent } from './grid-maintenance/grid-maintenance.co
     FormMaintenanceComponent,
     GridMaintenanceComponent
   ],
-  imports: [
-    CommonModule,
-    MaintenanceRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        MaintenanceRoutingModule,
+        MatIconModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatTableModule
+    ]
 })
 export class MaintenanceModule { }
