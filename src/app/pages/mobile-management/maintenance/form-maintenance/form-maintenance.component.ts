716dc7960adabcfe0ba0749c89a99e2b5d8d7d24
Mijail Gonzalez <mgonzalez@geotech.com.co>
2022-02-15 17:50:51 -0500

    ajuste en formulario

--- src/app/pages/mobile-management/maintenance/form-maintenance/form-maintenance.component.ts
+++ src/app/pages/mobile-management/maintenance/form-maintenance/form-maintenance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-form-maintenance',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-maintenance.component.scss']
 })
 export class FormMaintenanceComponent implements OnInit {
-
-  constructor() { }
+  public form: FormGroup;
+  constructor(
+      private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+      this.createForm();
+  }
+
+  private createForm(): void {
+      this.form = this.fb.group({
+          sms: this.fb.array([''])
+      });
+  }
+
+  public addSms(): void {
+     this.smsArray.push(this.fb.control(''));
+  }
+
+  public removeSms(id: number) {
+      this.smsArray.removeAt(id);
+  }
+
+  get smsArray(): FormArray {
+      return this.form.get('sms') as FormArray;
   }
 
 }
