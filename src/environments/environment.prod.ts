f5171b7ed6e476c788211c27b39e9554ce7a5386
Arturo J. Avila <arturoavila71@gmail.com>
2022-09-21 18:08:27 -0500

    Cambios modulo de menu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,10 +5,10 @@ export const environment = {
     // baseUrl: 'https://newapi.geotech.com.co/',
 
     /*Api para pruebas en local*/
-    urlApi: 'http://192.168.190.56:3000/api/v1/',
-    baseUrl: 'http://192.168.190.56:3000/',
+    // urlApi: 'http://192.168.190.56:3000/api/v1/',
+    // baseUrl: 'http://192.168.190.56:3000/',
 
     /*Api para pruebas en mi local*/
-    // urlApi: 'http://localhost:3000/api/v1/',
-    // baseUrl: 'http://localhost:3000/',
+    urlApi: 'http://localhost:3000/api/v1/',
+    baseUrl: 'http://localhost:3000/',
 };
