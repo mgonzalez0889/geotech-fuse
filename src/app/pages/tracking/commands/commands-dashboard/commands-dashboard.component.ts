53b1f08b4130896c80a1bb5d9a5f5d830177a73e
jordan pautt <pauttjordan7@gmail.com>
2022-12-30 11:46:12 -0500

    feat:change module command and add images form

--- src/app/pages/tracking/commands/commands-dashboard/commands-dashboard.component.ts
+++ src/app/pages/tracking/commands/commands-dashboard/commands-dashboard.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CommandsService } from 'app/core/services/api/commands.service';
-import { FleetsService } from 'app/core/services/api/fleets.service';
-import { MobileService } from 'app/core/services/api/mobile.service';
-import { ConfirmationService } from 'app/core/services/confirmation/confirmation.service';
 import { interval, Subject } from 'rxjs';
 import { TranslocoService } from '@ngneat/transloco';
 import { ToastAlertService } from '@services/toast-alert/toast-alert.service';
-import { takeUntil, delay } from 'rxjs/operators';
+import { takeUntil, delay, mergeMap, filter } from 'rxjs/operators';
 import { IButtonOptions, IOptionTable, IStateHeaders } from '@interface/index';
+import { CommandsService } from '@services/api/commands.service';
+import { ConfirmationService } from '@services/confirmation/confirmation.service';
 
 @Component({
   selector: 'app-commands-dashboard',
@@ -15,27 +13,11 @@ import { IButtonOptions, IOptionTable, IStateHeaders } from '@interface/index';
   styleUrls: ['./commands-dashboard.component.scss'],
 })
 export class CommandsDashboardComponent implements OnInit, OnDestroy {
-  public show: boolean;
   public openedDrawer = false;
-  public today = new Date();
-  public month = this.today.getMonth();
-  public year = this.today.getFullYear();
-  public day = this.today.getDate();
   public commandsData: any[] = [];
-  public searchPlate: any;
-  public searchFleets: any;
-  public validationFleet: number = 0;
-  public allSelectedMobiles: boolean = false;
-  public allSelectedFleets: boolean = false;
-  public typeCommands: any;
-  public initialDate: Date = new Date(this.year, this.month, this.day);
-  public finalDate: Date = new Date(this.year, this.month, this.day);
-  public selectedPlates = [];
-  public selectedTypeCommand: number;
+  public initialDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+  public finalDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
   public selectedFleets = [];
-  public mobiles: any = [];
-  public fleets: any = [];
-  public intervallTimer = interval(20000);
   public dataFilter: string = '';
   public optionsTable: IOptionTable[] = [
     {
@@ -101,136 +83,50 @@ export class CommandsDashboardComponent implements OnInit, OnDestroy {
     icon: 'refresh',
     text: 'commands.tablePage.resendCommand',
     action: (data) => {
+      console.log(data);
+
       this.resendCommand(data.plate, data.typecommand_id);
     },
   };
   private unsubscribe$ = new Subject<void>();
   constructor(
-    private commandsService: CommandsService,
-    private mobilesService: MobileService,
-    private fleetService: FleetsService,
-    private confirmationService: ConfirmationService,
+    private commandService: CommandsService,
     private translocoService: TranslocoService,
-    private toastAlertService: ToastAlertService
+    private toastAlert: ToastAlertService,
+    private confirmationService: ConfirmationService,
   ) { }
 
   ngOnInit(): void {
-    this.getTypeCommand();
     this.getSentCommands();
-    this.getFleets();
-    this.getMobiles();
-    this.intervallTimer
+
+    const intervallTimer$ = interval(20000);
+    intervallTimer$
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(() =>
         this.getSentCommands()
       );
+
     this.translocoService.langChanges$
       .pipe(takeUntil(this.unsubscribe$), delay(500))
       .subscribe(() => {
         this.getSentCommands();
-        this.getTypeCommand();
       });
   }
 
-  /**
-   * @description: Funcion del filtro en la tabla
-   */
-  filterTable(event: Event): void {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataFilter = filterValue.trim().toLowerCase();
-  }
-
-  /**
-   * @description: Armar los datos para poder mandar el comando desde la opcion de reenviar
-   */
-  public resendCommand(plate: any, typeCommand: number): void {
-    const commands = {
-      validationFleet: this.validationFleet,
-      fleets: this.selectedFleets,
-      plates: [plate],
-      command: typeCommand,
-    };
-    this.sendCommandsToDevice(commands);
-  }
-  /**
-   * @description: Armar los datos para poder mandar el comando
-   */
-  public sentCommands(): void {
-    this.selectedPlates = [];
-    this.selectedFleets = [];
-    this.mobiles.forEach((x) => {
-      if (x.selected) {
-        this.selectedPlates.push(x.plate);
-      }
-    });
-    this.fleets.forEach((x) => {
-      if (x.selected) {
-        this.selectedFleets.push(x.id);
-      }
-    });
-    const commands = {
-      validationFleet: this.validationFleet,
-      fleets: this.selectedFleets,
-      plates: this.selectedPlates,
-      command: this.selectedTypeCommand,
-    };
-    this.sendCommandsToDevice(commands);
-  }
-
-  /**
-   * @description: Funcion valida si estamos enviando comandos a una flota o un vehiculo
-   */
-  public typeOfSelection(event: any): void {
-    this.validationFleet = event.index;
-  }
-  /**
-   * @description: Funcion de seleccionar todo
-   */
-  public setAll(completed: boolean, type: string): any {
-    if (type === 'mobiles') {
-      this.allSelectedMobiles = completed;
-      if (this.mobiles == null) {
-        return;
-      }
-      this.mobiles.forEach((x) => {
-        x.selected = completed;
-      });
-    } else if ('fleets') {
-      this.allSelectedFleets = completed;
-      if (this.fleets == null) {
-        return;
-      }
-      this.fleets.forEach(t => (t.selected = completed));
-    }
-  }
-  /**
-   * @description: Funcion de validar si esta todo seleccionado
-   */
-  public someComplete(type: string): boolean {
-    if (type === 'mobiles') {
-      if (this.mobiles == null) {
-        return false;
-      }
-      return (
-        this.mobiles.filter(t => t.selected).length > 0 &&
-        !this.allSelectedMobiles
-      );
-    } else if ('fleets') {
-      if (this.fleets == null) {
-        return false;
-      }
-      return (
-        this.fleets.filter(t => t.selected).length > 0 &&
-        !this.allSelectedFleets
-      );
-    }
-  }
   /**
    * @description: Destruye el observable
    */
   ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+  }
+
+  /**
+   * @description: Funcion del filtro en la tabla
+   */
+  public filterTable(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataFilter = filterValue.trim().toLowerCase();
   }
 
   /**
@@ -241,7 +137,7 @@ export class CommandsDashboardComponent implements OnInit, OnDestroy {
       dateInit: this.initialDate.toLocaleDateString() + ' 00:00:00',
       dateEnd: this.finalDate.toLocaleDateString() + ' 23:59:59',
     };
-    this.commandsService.postSearchCommandsSend(date)
+    this.commandService.postSearchCommandsSend(date)
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe((response) => {
         if (response.data_count) {
@@ -253,82 +149,40 @@ export class CommandsDashboardComponent implements OnInit, OnDestroy {
       });
   }
 
-  /**
-   * @description: Muestra los tipos de comandos del cliente
-   */
-  private getTypeCommand(): void {
-    this.commandsService.getTypeCommands()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((data) => {
-        this.typeCommands = data.data;
-      });
-  }
-  /**
-   * @description: Obtiene los vehiculos del cliente
-   */
-  private getMobiles(): void {
-    this.mobilesService.getMobiles()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((data) => {
-        this.mobiles = data?.data?.map((x) => {
-          x['selected'] = false;
-          return x;
-        });
-      });
-  }
-  /**
-   * @description: Obtiene las flotas del cliente
-   */
-  private getFleets(): void {
-    this.fleetService.getFleets()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((data) => {
-        this.fleets = data?.data?.map((x) => {
-          x['selected'] = false;
-          return x;
-        });
-      });
-  }
-
-
-  /**
-   * @description: Funcion de enviar comandos
-   */
-  private sendCommandsToDevice(commands: any): void {
-    if (commands.fleets.length || commands.plates.length) {
-      let confirmation = this.confirmationService.open({
-        title: this.translocoService.translate('commands.commandAlert.titleAlert'),
-        message:
-          this.translocoService.translate('commands.commandAlert.messageAlert'),
-      });
-      confirmation.afterClosed().subscribe((result) => {
-        if (result === 'confirmed') {
-          this.show = true;
-          this.commandsService
-            .postCommandsSend(commands)
-            .subscribe((data) => {
-              if (data.code === 200) {
-                this.show = false;
-                confirmation = this.confirmationService.open({
-                  title: this.translocoService.translate('commands.commandAlert.titleAlert'),
-                  message: this.translocoService.translate('commands.commandAlert.successCommand'),
-                });
-              } else {
-                confirmation = this.confirmationService.open({
-                  title: this.translocoService.translate('commands.commandAlert.titleAlert'),
-                  message:
-                    this.translocoService.translate('commands.commandAlert.alertMessageError'),
-                });
-              }
-              this.getSentCommands();
-            });
+  public sendCommand(commandData: any): void {
+    const confirmation = this.confirmationService.open({
+      title: this.translocoService.translate('commands.commandAlert.titleAlert'),
+      message:
+        this.translocoService.translate('commands.commandAlert.messageAlert'),
+    });
+    confirmation.afterClosed()
+      .pipe(
+        filter(result => result === 'confirmed'),
+        mergeMap(() => this.commandService.postCommandsSend(commandData))
+      )
+      .subscribe(({ code }) => {
+        if (code === 200) {
+          this.toastAlert.toasAlertSuccess({
+            message: 'commands.commandAlert.successCommand'
+          });
+        } else {
+          this.toastAlert.toasAlertWarn({
+            message: 'commands.commandAlert.alertMessageError'
+          });
         }
       });
-    } else {
-      this.toastAlertService.toasAlertWarn({
-        message: 'commands.commandAlert.alertMessageSelectionError'
-      });
-    }
   }
 
+  /**
+   * @description: Armar los datos para poder mandar el comando desde la opcion de reenviar
+   */
+  private resendCommand(plate: any, typeCommand: number): void {
+    const commands = {
+      validationFleet: 0,
+      fleets: this.selectedFleets,
+      plates: [plate],
+      command: typeCommand,
+    };
+    this.sendCommand(commands);
+  }
 }
--- src/app/pages/tracking/commands/form-commands/form-commands.component.ts
+++ src/app/pages/tracking/commands/form-commands/form-commands.component.ts
@@ -1,4 +1,13 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { takeUntil, filter, mergeMap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { CommandsService } from '@services/api/commands.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MobileService } from '@services/api/mobile.service';
+import { FleetsService } from '@services/api/fleets.service';
+import { ConfirmationService } from '@services/confirmation/confirmation.service';
+import { TranslocoService } from '@ngneat/transloco';
+import { ToastAlertService } from '@services/toast-alert/toast-alert.service';
 
 @Component({
   selector: 'app-form-commands',
@@ -7,10 +16,55 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class FormCommandsComponent implements OnInit {
   @Output() emitCloseForm = new EventEmitter<void>();
+  public mobilesData: any[] = [];
+  public fleetsData: any[] = [];
+  public typeCommands: any[] = [];
+  public formCommands: FormGroup = this.formBuilder.group({});
+  private unsubscribe$ = new Subject<void>();
 
-  constructor() { }
+  constructor(
+    private commandService: CommandsService,
+    private mobileService: MobileService,
+    private fleetService: FleetsService,
+    private formBuilder: FormBuilder,
+    private translocoService: TranslocoService,
+    private confirmationService: ConfirmationService,
+    private toastAlert: ToastAlertService
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
+    this.mobileService.getMobiles()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(({ data }) => {
+        this.mobilesData = data || [];
+      });
+
+    this.fleetService.getFleets()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(({ data }) => {
+        this.fleetsData = data || [];
+      });
+
+    this.commandService.getTypeCommands()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(({ data }) => {
+        this.typeCommands = data || [];
+      });
+  }
+
+  sendCommand(): void {
+    const formValue = this.formCommands.value;
+  }
+
+  private buildForm(): void {
+    this.formCommands = this.formBuilder.group({
+      fleets: [[]],
+      plates: [[]],
+      validationFleet: [0],
+      typeCommandId: ['', Validators.required]
+    });
   }
 
 }
